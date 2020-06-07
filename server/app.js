4cdd2da938c88ad80ac05dce387470975a89fd67
ArtGMlg <art6444.om@gmail.com>
2020-06-07 20:23:12 +0600

    Design optimizations

--- server/app.js
+++ server/app.js
@@ -41,4 +41,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(5000, '0.0.0.0', function() {
+    console.log('Listening to port:  ' + 5000);
+});
+
 module.exports = app;
